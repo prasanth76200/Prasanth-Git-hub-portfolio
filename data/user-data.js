6c4f09a5b15f279ccd4939a6879f7eb40775775a
prasanth76200 <prasanth76200@gmail.com>
2024-08-04 10:53:06 +0530

    first commit

---
+++ data/user-data.js
@@ -0,0 +1,13 @@
+export const userData = {
+  githubUser: 'prasanth76200',
+  devUsername: "prasanth_k",
+  github: 'https://github.com/prasanth76200',
+  // facebook: '',
+  linkedIn: 'https://www.linkedin.com/in/followprasanth/',
+  twitter: 'https://twitter.com/here_prasanth01',
+  // stackOverflow: '',
+  leetcode: "https://leetcode.com/prasanth76200/",
+  resume: "https://drive.google.com/file/d/10_aFNzEyd61a3QEbT7KkX4-jCzwNiRDy/view?usp=sharing",
+  skills: ['Java', 'Go', 'JavaScript', 'MySQL', 'Spring Boot MVC', 'Swagger', 'Tailwind CSS', 'React.js', 'MongoDB', 'Docker', 'Kubernetes', 'Kafka', 'Eurkea', 'Zipkin', 'Keycloak', 'Linux', 'GitHub', 'Postman'],
+  timezone: '+6'
+};
